e5f6d61c73f2e1d5a5d7a05b9f139d51be1cfeb9
satol <satol@yahoo.com>
2014-09-14 01:38:53 -0700

    2

--- OperatorSimplifyOGCTest/stolstov/OperatorSimplifyOGCTest.java
+++ OperatorSimplifyOGCTest/stolstov/OperatorSimplifyOGCTest.java
@@ -1,10 +1,28 @@
 package stolstov;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.esri.core.geometry.OperatorImportFromWkt;
+import com.esri.core.geometry.OperatorSimplifyOGC;
+
 public final class OperatorSimplifyOGCTest {
 
-	public static void main(String[] args) {
-
-
+	public static void main(String[] args) throws IOException {
+		bytes[] bytes = Files.readAllBytes(Paths.get(args[0]));
+		String text = new String(bytes, "US-ASCII");
+		Geometry g = OperatorImportFromWkt.local().executeOGC(0,  text,  null);
+		System.out.println(args[0] + " : " + g.getGeometryType() + 
+				" " + ((MultiPath)g).getPointCount());
+		System.out.print("Checking if simple... ");
+		boolean isSimple = OperatorSimplifyOGC.local().isSimpleOGC(g,  null,  true, null, null);
+		System.out.println(isSimple);
+		System.out.print("Running siplify operation... ");
+		Geometry gSimple = OperatorSimplifyOGC.local().execute(g, null, true, null);
+		System.out.println("Done");
+		System.out.print("Running siplify operation (should be simple now)... ");
+		isSimple = OperatorSimplifyOGC.local().isSimpleOGC(gSimple,  null,  true, null, null);
+		System.out.println(isSimple);
 	}
 
 }
